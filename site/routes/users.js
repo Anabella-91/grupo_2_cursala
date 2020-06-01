3f6318e982820d4b7fed94e679ae0ebbce7e41db
Anabella <ana.da.rocha@hotmail.com>
2020-06-01 09:11:06 -0300

    Se sigue trabajando con las rutas, login y registro de usuarios

--- site/routes/users.js
+++ site/routes/users.js
@@ -7,8 +7,9 @@ const controller = require('../controllers/usersController');
 /* user registro . */
 router.get('/registro', controller.registro);
 router.post('/registro',[
-    check('name').isLength({min:2}).withMessage('Tu nombre debe contener al menos dos letras'),
-    check('email').isEmail().withMessage('Email invalido')
+    check('name').notEmpty().withMessage('Tu nombre debe contener al menos dos letras'),
+    check('email').isEmail().withMessage('Email invalido'),
+    check('password').notEmpty().withMessage('La contrasena debe contener 8 caracteres')
 ], controller.registroUser);
 
 /* user login . */
@@ -17,6 +18,12 @@ router.post('/login',[
     check('email').isEmail().withMessage('Email invalido'),
     check('password').isLength({min:8}).withMessage('La contrasena debe contener 8 caracteres')
 ], controller.processLogin);
+
+/* user edit */
+router.get('/edit/:idUser', controller.edit);
+router.put('/edit', function(req, res){
+    res.send('Fui por put');
+});
 
 
 /* user carrito . */
