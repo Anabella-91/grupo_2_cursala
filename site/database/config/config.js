8452a8fe5f5bed867a90a21b6228317951f92fd5
Anabella <ana.da.rocha@hotmail.com>
2020-06-26 21:07:50 -0300

    Agregando paquete dotenv

--- site/database/config/config.js
+++ site/database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": "cursala2020",
-    "database": "cursala_database",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PASS,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql",
     "operatorsAliases": 0
   },
