b38e5725287eca725b93e6b1d4a5df3350e4da48
Anabella <ana.da.rocha@hotmail.com>
2020-06-29 09:50:24 -0300

    Actualizando rutas de usuarios y productos

--- site/routes/products.js
+++ site/routes/products.js
@@ -5,19 +5,18 @@ const { check, validationResult, body} = require('express-validator');
 
 
 /* Formulario de creacion de productos */
-router.get('/create', controller.formCreate);
-router.post('/', controller.save);
-
+router.get('/create', controller.createProduct);
+router.post('/', controller.saveProduct);
 
 /* Detalle de un producto */
-router.get('/:id', controller.detail);
+router.get('/:id', controller.detailProduct);
 
 /* Actualizacion de producto para el admin */ 
-router.get('/:id', controller.edit);
-router.post('/:id', controller.update);
+router.get('/:id', controller.editProduct);
+router.post('/:id', controller.updateProduct);
 
 /* Accion de borrado de producto */
-router.delete('/delete/:id', controller.delete);
+router.delete('/delete/:id', controller.deleteProduct);
 
 
 module.exports = router;
