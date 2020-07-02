1a8a2cd33a55ac2ad81454f696a1e896818862b3
Maite <maite.v@outlook.com>
2020-07-02 14:22:52 -0300

    Se agrego js al formulario de contacto para que actue como ventana modal

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,6 +1,14 @@
 window.addEventListener("load", function(){
-    let liMenu = document.querySelectorAll("d-none");
-    for(let i = 0; i < liMenu.length; i++) {
-        liMenu.style.display = "none";
-    }
+    let btnOpenContacto = document.getElementById("openContacto");
+    let modalContacto = document.getElementById("openModal");
+    let btnCloseContacto = document.getElementById("closeContacto");
+
+    btnOpenContacto.addEventListener("click", function(){
+        modalContacto.style.display ="flex";
+    })
+    btnCloseContacto.addEventListener("click", function(){
+        modalContacto.style.display = "none";
+    });
+
+
 });
