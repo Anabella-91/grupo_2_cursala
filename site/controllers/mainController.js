7fc858b7146f68f0a8114491617d18881be6ff01
Maite <maite.v@outlook.com>
2020-07-27 22:24:40 -0300

    Armado el modelo de mensajes, se adiciono la funcionalidad crear en el controlador

--- site/controllers/mainController.js
+++ site/controllers/mainController.js
@@ -1,4 +1,4 @@
- db = require('../database/models');
+const db = require('../database/models');
 
 module.exports = {
 	home:function (req,res, next){
@@ -12,24 +12,15 @@ module.exports = {
     landing:function (req,res,next){
         res.render('landing', { title: 'Cursala | Landing'});
     },
-    mensaje: function (req, res, next){
-        let mensajeForm = {
+    mensaje: function (req, res){
+        db.Mensaje.create({
             nombre: req.body.nombre,
             email: req.body.email,
             telefono: req.body.telefono,
             asunto: req.body.asunto,
-            canalContacto: req.body.canalContacto,
+            medio_contacto: req.body.canalContacto,
             mensaje: req.body.mensaje
-        };
-        let mensajes = [];
-        let archivoMensajes = fs.readFileSync("mensajes.json", {encoding: "utf-8"});
-        if (archivoMensajes != ""){
-            mensajes = JSON.parse(archivoMensajes);
-        } 
-        mensajes.push(mensajeForm);
-        let mensajeJSON = JSON.stringify(mensajes);
-        fs.writeFileSync("mensajes.json", mensajeJSON);
-        
-        res.redirect('/');
+        })
+        res.redirect('/home');
     }
 };
