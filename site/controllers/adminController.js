5a50b07d9f75970a01933a85db6b489b6ee9dc5d
Maite <maite.v@outlook.com>
2020-08-01 16:51:19 -0300

    Arreglo del heigth de la vista de mensajes de administrador, se adiciono un ORDER para visualizar primero los mesajes mas nuevos

--- site/controllers/adminController.js
+++ site/controllers/adminController.js
@@ -2,7 +2,10 @@ const db = require('../database/models');
 
 module.exports = {
     mostrar: function (req, res) {
-        let mensajes = db.Mensaje.findAll()
+        let mensajes = db.Mensaje.findAll({
+            order: [ ['id', 'DESC'] ],
+            limit: 3
+        })
         .then(function(mensajes){
             return res.render('admin_mensajes', {title: 'Admin | mensajes', mensajes: mensajes});
         })
