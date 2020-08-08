7f539561a1cf8981d15b0b2c87f999892a4e9534
Maite <maite.v@outlook.com>
2020-08-08 19:54:57 -0300

    Se actualizo el script del funcionamiento del menu

--- public/javascripts/menu.js
+++ public/javascripts/menu.js
@@ -1,9 +1,21 @@
+let liMenu = document.querySelectorAll(".liMenu");
+
+if(window.screen.width < 650) {
+    for (let i = 0; i < liMenu.length; i++) {
+        liMenu[i].classList.add("none");
+    }
+}
 window.addEventListener("load", function(){
-    let liMenu = document.querySelectorAll(".liMenu");
+
     let botonMenu = document.getElementById("boton-menu");
+
     botonMenu.addEventListener("click", function(){
         for(let i = 0; i < liMenu.length; i++) {
+            if(window.screen.width > 650) {
+            liMenu[i].classList.toggle("none")
+            } else {
             liMenu[i].classList.toggle("none");
+            }
         }
     })
 })
